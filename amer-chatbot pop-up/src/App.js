b49fae9468456a9ebf1290bf65d5550f2fb7e8c9
Ameen Ahmed <aa5762@g.rit.edu>
2025-08-20 21:50:09 +0400

    Chat-Bot Pop-up option

---
+++ amer-chatbot pop-up/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AmerSmartAssistant from './chatbot';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AmerSmartAssistant />
+    </div>
+  );
+}
+
+export default App;
